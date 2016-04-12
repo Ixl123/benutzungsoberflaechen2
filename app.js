171a4937b40ed8c0e0b0e58e4121d979db24e50c
Jacob Cofman <jcofman@Jacobs-MBP.fritz.box>
2016-04-12 17:39:13 +0200

    added modeler support and css files to the dependencies

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ var viewer = new BpmnViewer({ container: '#canvas' });
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        viewer.importXML(xhr.response, function(err) {
+  if (xhr.readyState === 4) {
+    viewer.importXML(xhr.response, function(err) {
 
-          if (!err) {
-            console.log('success!');
-            viewer.get('canvas').zoom('fit-viewport');
-          } else {
-            console.log('something went wrong:', err);
-          }
-        });
-    }
+      if (!err) {
+        console.log('success!');
+        viewer.get('canvas').zoom('fit-viewport');
+      } else {
+        console.log('something went wrong:', err);
+      }
+    });
+  }
 };
 
 xhr.open('GET', '../resources/pizza-collaboration.bpmn', true);
